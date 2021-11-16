23c09579f5a0d2ef0192ba73c3fb64aae6726635
4lbatr0s <serhatoner@protonmail.com>
2021-11-16 20:29:03 +0300

    creates models, routes, views, ejs files

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,7 +1,7 @@
  const Course = require('../models/Course')
 
  exports.createCourse = async (req,res) => {
-     const course = await Course.create(req.body);
+     const course = await Course.create(req.body); //we catch it by req.body
      try { //we want to catch errors if they come out.
         res.status(200).json({
             status: 'success',
@@ -27,6 +27,24 @@
     res.status(201).render('courses', {
         courses, //send coureses to courses.ejs and render courses.ejs
         page_name: 'courses',
+    })
+    } catch (error) {
+        res.status(400).json({
+            status:'fail',
+            error,
+        })
+    }
+   
+}
+
+
+
+exports.getCourse = async (req,res) => { //we catch 
+    const courses = await Course.findById(_id:req.params.id); //get the course by it's ID.
+    try {
+    res.status(201).render('course-single', {
+        courses, //send coureses to courses.ejs and render courses.ejs
+        page_name: 'course-single',
     })
     } catch (error) {
         res.status(400).json({
